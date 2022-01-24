c7bb22dad5a2856428bfe645889b91c53ada059d
bemedel <bemedel@uw.edu>
2022-01-23 23:45:24 -0800

    added comments

--- chatter/src/App.js
+++ chatter/src/App.js
@@ -7,18 +7,18 @@ function App() {
         <div className="logo" />
         <span className="title">Chatter</span>
       </header>
-      <div className="chat-container">
-        <div className="chat-bubble">
+      <div className="chat-container"> /*Contains all chat bubbles for styling purposes*/
+        <div className="chat-bubble"> /*first chat bubble*/
           hello this is a message
         </div>
-        <div className="chat-bubble">
+        <div className="chat-bubble"> /*second chat bubble*/
           hi
         </div>
-        <div className="chat-bubble">
+        <div className="chat-bubble"> /*third chat bubble*/
           yup
         </div>
       </div>
-      <footer className="footer">
+      <footer className="footer"> /* this has the text box and send button*/
         <input className="text-input" />
         <button className="send">
         </button>
